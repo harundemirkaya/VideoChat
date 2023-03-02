b19efed20904016f4fe4fcf860fa25e95aa1173f
harundemirkaya <harundmrk@gmail.com>
2023-03-02 15:25:45 +0300

    Initial Commit

---
+++ VideoChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoChat
+//
+//  Created by Harun Demirkaya on 2.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
