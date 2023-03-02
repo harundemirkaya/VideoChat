9d200afee99d85bc7ff64bf984c2c31318d90720
harundemirkaya <harundmrk@gmail.com>
2023-03-02 15:37:38 +0300

    Initial Project

--- VideoChat/Views/ViewController.swift
+++ VideoChat/Views/ViewController.swift
@@ -9,11 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: -LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
     }
-
-
 }
 
